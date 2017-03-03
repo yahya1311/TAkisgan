37ec336396acad5846d541f98ce65ca9c6c33870
Yahya1311 <yahyasemm@gmail.com>
2017-03-03 09:41:09 +0700

    Ditambahkan atribut posisi tangan

--- Progres_KinectHandTracking/KinectHandTracking/KinectHandTracking/KinectHandTracking/MainWindow.xaml.cs
+++ Progres_KinectHandTracking/KinectHandTracking/KinectHandTracking/KinectHandTracking/MainWindow.xaml.cs
@@ -34,6 +34,10 @@ namespace KinectHandTracking
         //Tangan Kanan
         private double x = 0, y = 0, z = 0, deltax = 0, deltay = 0, deltaz = 0, alpha2 = 0, tmpx = 0, tmpy = 0, tmpz = 0;
         private int[] kuant2 = new int[999];
+        //Penentuan Posisi
+        private double leher = 0, tengah = 0;
+        //Posisinya
+        private string tanganKanan = "", tanganKiri = "";
 
         //Penanda
         private int flag = 0;
@@ -112,11 +116,16 @@ namespace KinectHandTracking
                             if (body.IsTracked)
                             {
                                 // Identifikasi Tulang
-                                Joint handRight = body.Joints[JointType.HandRight];
-                                Joint thumbRight = body.Joints[JointType.ThumbRight];
-                                Joint handLeft = body.Joints[JointType.HandLeft];
-                                Joint thumbLeft = body.Joints[JointType.ThumbLeft];
 
+                                //Joint shoulderRight = body.Joints[JointType.ShoulderRight];
+                                Joint handRight = body.Joints[JointType.HandRight];
+                                //Joint thumbRight = body.Joints[JointType.ThumbRight];
+                                //Joint shoulderLeft = body.Joints[JointType.ShoulderLeft];
+                                Joint handLeft = body.Joints[JointType.HandLeft];
+                                //Joint thumbLeft = body.Joints[JointType.ThumbLeft];
+                                Joint neck = body.Joints[JointType.Neck];
+                                Joint spineMid = body.Joints[JointType.SpineMid];
+                  
                                 // Menggambar Skeleton
                                 canvas.DrawSkeleton(body, _sensor.CoordinateMapper);
 
@@ -173,31 +182,48 @@ namespace KinectHandTracking
                                 #endregion
 
                                 #region Untuk Membuat Dataset Baru
-                                // Jika tangan kanan & kiri terbuka
+                                // Jika Tangan Kanan & Kiri Terbuka
                                 if (body.HandRightState == HandState.Open && body.HandLeftState == HandState.Open)
                                 {
-                                    //hitung-hitung
-                                    a = (handLeft.Position.X); b = (handLeft.Position.Y); c = (handLeft.Position.Z);
-                                    x = (handRight.Position.X); y = (handRight.Position.Y); z = (handRight.Position.Z);
+                                    // Koordinat Tangan
+                                    a = (handLeft.Position.X); b = (handLeft.Position.Y); //c = (handLeft.Position.Z);
+                                    x = (handRight.Position.X); y = (handRight.Position.Y); //z = (handRight.Position.Z);
 
-                                    //data atau frame pertama
+                                    // Frame Pertama
                                     if (flag == 0)
                                     {
-                                        deltaa = a; deltab = b; deltac = c;
-                                        deltax = x; deltay = y; deltaz = z;
+                                        deltaa = a; deltab = b; //deltac = c;
+                                        deltax = x; deltay = y; //deltaz = z;
                                         flag = 1;
                                     }
                                     else
                                     {
-                                        deltaa = a - tmpa; deltab = b - tmpy; deltac = c - tmpc;
-                                        deltax = x - tmpx; deltay = y - tmpy; deltaz = z - tmpz;
+                                        deltaa = a - tmpa; deltab = b - tmpy; //deltac = c - tmpc;
+                                        deltax = x - tmpx; deltay = y - tmpy; //deltaz = z - tmpz;
                                     }
-
-                                    tmpa = a; tmpb = b; tmpc = c;
-                                    tmpx = x; tmpy = y; tmpz = z;
+                                    tmpa = a; tmpb = b; //tmpc = c;
+                                    tmpx = x; tmpy = y; //tmpz = z;
 
                                     if (i < 40)
                                     {
+                                        if (i == 1)
+                                        {
+                                            leher = (neck.Position.Y);
+                                            tengah = (spineMid.Position.Y);
+                                        }
+
+                                        if (i == 19)
+                                        {
+                                            //b handleft tanganKiri //y handright tanganKanan
+                                            if (b > leher) tanganKiri = "Atas Kepala";
+                                            else if (b < tengah) tanganKiri = "Perut";
+                                            else tanganKiri = "Dada";
+
+                                            if (y > leher) tanganKanan = "Atas Kepala";
+                                            else if (y < tengah) tanganKanan = "Perut";
+                                            else tanganKanan = "Dada";
+                                        }
+
                                         //ekstraksi fitur dinamis
                                         //tangan kiri
                                         if (deltaa >= 0 && deltab >= 0)
@@ -235,6 +261,7 @@ namespace KinectHandTracking
                                         else if (alpha2 > 44) { kuant2[i] = 2; }
                                         else { kuant2[i] = 1; }
 
+                                        //write ke csv
                                         if (i == 38)
                                         {
                                             var stringkuant0 = kuant1[0].ToString(); var stringkuant1 = kuant1[1].ToString(); var stringkuant2 = kuant1[2].ToString();
@@ -263,8 +290,9 @@ namespace KinectHandTracking
                                             var stringkuant69 = kuant2[30].ToString(); var stringkuant70 = kuant2[31].ToString(); var stringkuant71 = kuant2[32].ToString();
                                             var stringkuant72 = kuant2[33].ToString(); var stringkuant73 = kuant2[34].ToString(); var stringkuant74 = kuant2[35].ToString();
                                             var stringkuant75 = kuant2[36].ToString(); var stringkuant76 = kuant2[37].ToString(); var stringkuant77 = kuant2[38].ToString();
+                                            var stringtangankiri = tanganKiri; var stringtangankanan = tanganKanan;
 
-                                            var newLine = string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},{15},{16},{17},{18},{19},{20},{21},{22},{23},{24},{25},{26},{27},{28},{29},{30},{31},{32},{33},{34},{35},{36},{37},{38},{39},{40},{41},{42},{43},{44},{45},{46},{47},{48},{49},{50},{51},{52},{53},{54},{55},{56},{57},{58},{59},{60},{61},{62},{63},{64},{65},{66},{67},{68},{69},{70},{71},{72},{73},{74},{75},{76},{77}",
+                                            var newLine = string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},{15},{16},{17},{18},{19},{20},{21},{22},{23},{24},{25},{26},{27},{28},{29},{30},{31},{32},{33},{34},{35},{36},{37},{38},{39},{40},{41},{42},{43},{44},{45},{46},{47},{48},{49},{50},{51},{52},{53},{54},{55},{56},{57},{58},{59},{60},{61},{62},{63},{64},{65},{66},{67},{68},{69},{70},{71},{72},{73},{74},{75},{76},{77},{78},{79}",
                                                 stringkuant0, stringkuant1, stringkuant2, stringkuant3, stringkuant4, stringkuant5, stringkuant6, stringkuant7, stringkuant8,
                                                 stringkuant9, stringkuant10, stringkuant11, stringkuant12, stringkuant13, stringkuant14, stringkuant15, stringkuant16, stringkuant17,
                                                 stringkuant18, stringkuant19, stringkuant20, stringkuant21, stringkuant22, stringkuant23, stringkuant24, stringkuant25, stringkuant26,
@@ -273,7 +301,7 @@ namespace KinectHandTracking
                                                 stringkuant45, stringkuant46, stringkuant47, stringkuant48, stringkuant49, stringkuant50, stringkuant51, stringkuant52, stringkuant53,
                                                 stringkuant54, stringkuant55, stringkuant56, stringkuant57, stringkuant58, stringkuant59, stringkuant60, stringkuant61, stringkuant62,
                                                 stringkuant63, stringkuant64, stringkuant65, stringkuant66, stringkuant67, stringkuant68, stringkuant69, stringkuant70, stringkuant71,
-                                                stringkuant72, stringkuant73, stringkuant74, stringkuant75, stringkuant76, stringkuant77);
+                                                stringkuant72, stringkuant73, stringkuant74, stringkuant75, stringkuant76, stringkuant77, stringtangankiri, stringtangankanan);
 
                                             //memasukkan ke dalam baris
                                             csv.AppendLine(newLine);
